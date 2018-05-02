80db672c26adbf37aa4de744e59c03532289b312
Vegetto <github@angelnu.com>
2018-05-03 01:30:25 +0200

    Add commit versions (#7)

--- build.sh
+++ build.sh
@@ -16,6 +16,12 @@ cd $(dirname $0)
 : ${BASE:="alpine"}
 : ${REPO:="angelnu/keepalived"}
 : ${QEMU_VERSION:="v2.11.1"}
+
+if [ "x$TAG" = "xlatest" ]; then
+  ALIAS_ARCH_TAG="${TAG}-${DOCKER_ARCH}"
+  ORG_TAG="$TAG"
+  TAG="$(git describe --always --dirty --tags || echo 0.1)"
+fi
 : ${ARCH_TAG:="${TAG}-${DOCKER_ARCH}"}
 
 
@@ -49,6 +55,11 @@ if [ "$BUILD" = true ] ; then
   fi
   echo "Building $REPO:$ARCH_TAG using base image $BASE and qemu arch $QEMU_ARCH"
   docker build -t $REPO:$ARCH_TAG --build-arg BASE=$BASE --build-arg arch=$QEMU_ARCH .
+  
+  if [ -n "$ALIAS_ARCH_TAG" ] ; then
+    docker tag $REPO:$ARCH_TAG $REPO:${ALIAS_ARCH_TAG}
+  fi
+  
 fi
 
 ##############################
@@ -56,6 +67,11 @@ fi
 if [ "$PUSH" = true ] ; then
   echo "PUSHING TO DOCKER: $REPO:$ARCH_TAG"
   docker push $REPO:$ARCH_TAG
+  
+  if [ -n "$ALIAS_ARCH_TAG" ] ; then
+    echo "PUSHING TO DOCKER: $REPO:${ALIAS_ARCH_TAG}"
+    docker push $REPO:${ALIAS_ARCH_TAG}
+  fi
 fi
 
 ###############################
@@ -77,4 +93,22 @@ if [ "$MANIFEST" = true ] ; then
   echo
   echo "Push manifest ${REPO}:${TAG}"
   docker manifest push ${REPO}:${TAG}
+  
+  if [ -n "$ALIAS_ARCH_TAG" ] ; then
+    for arch in $ARCHS; do
+      echo
+      echo "Pull ${REPO}:${ORG_TAG}-${arch}"
+      docker pull ${REPO}:${ORG_TAG}-${arch}
+
+      echo
+      echo "Add ${REPO}:${ORG_TAG}-${arch} to manifest ${REPO}:${ORG_TAG}"
+      docker manifest create --amend ${REPO}:${ORG_TAG} ${REPO}:${ORG_TAG}-${arch}
+      docker manifest annotate       ${REPO}:${ORG_TAG} ${REPO}:${ORG_TAG}-${arch} --arch ${arch}
+    done
+
+    echo
+    echo "Push manifest ${REPO}:${ORG_TAG}"
+    docker manifest push ${REPO}:${ORG_TAG}
+  fi
+  
 fi
